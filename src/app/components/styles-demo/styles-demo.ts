b86bba4d67fe64278f186bb7fa29e71c210a8ff2
antonysavache <89783847+antonysavache@users.noreply.github.com>
2025-09-07 18:56:13 +0200

    done

---
+++ src/app/components/styles-demo/styles-demo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-styles-demo',
+    templateUrl: './styles-demo.html',
+    standalone: true
+})
+export class StylesDemoComponent {
+  protected readonly title = 'Design System Demo';
+}
